7ed99de3e7d6fb8e776e4bdc52092ab47ea4a572
vacheret <vacheret>
2003-02-13 14:49:53 +0000

    specific files for PANAM

---
+++ src/TaPanamLinkDef.hh
@@ -0,0 +1,43 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//#pragma link C++ class ios;
+#pragma link C++ class TaThread+;
+#pragma link C++ class TaPanam+;
+//#pragma link C++ class TaCanvasWatchDog;
+//#pragma link C++ class TaCanvas;
+#pragma link C++ class TaPanamADevice;
+#pragma link C++ class TaPanamDevice;
+#pragma link C++ class TaStripChart;
+#pragma link C++ class TaADCCalib;
+#pragma link C++ class TaAnalysisManager;
+#pragma link C++ class TaDataBase;
+#pragma link C++ class TaBlind;
+#pragma link C++ class TaCutInterval;
+#pragma link C++ class TaCutList;
+#pragma link C++ class TaDebugAna;
+#pragma link C++ class TaDevice;
+#pragma link C++ class VaEvent;
+#pragma link C++ class TaEvent;
+#pragma link C++ class TaIResultsFile;
+#pragma link C++ class TaSimEvent;
+#pragma link C++ class TaFileName;
+#pragma link C++ class TaLabelledQuantity;
+#pragma link C++ class TaOResultsFile;
+#pragma link C++ class TaPairFromPair;
+#pragma link C++ class TaPairFromQuad;
+#pragma link C++ class TaRootRep;
+#pragma link C++ class TaRun;
+#pragma link C++ class TaStandardAna;
+#pragma link C++ class TaFeedbackAna;
+#pragma link C++ class TaPanamAna;
+#pragma link C++ class TaStatistics;
+#pragma link C++ class TaString;
+#pragma link C++ class VaAnalysis;
+#pragma link C++ class VaPair;
+
+
+
+#endif
