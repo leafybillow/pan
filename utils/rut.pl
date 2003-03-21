2ab7b3fd9b69a9d5a3a75547254bab2b574fca69
rsholmes <rsholmes>
2003-03-21 20:17:51 +0000

    Some minor fixes

--- utils/rut.pl
+++ utils/rut.pl
@@ -13,6 +13,7 @@ use Getopt::Long;
 
 $anadef = "standard";
 $comdef = "";
+$re = "";
 
 &dieusage() if @ARGV == 0;
 
@@ -22,71 +23,78 @@ GetOptions (
 	    "e|expr=s" => \$re,
 	    "h|help" => \$givehelp,
 	    "r|run=i" => \$run,
-	    );
+	   );
 
 &dieusage() if $givehelp;
 
 # Handle arguments required if no input files specified
 
 if (@ARGV == 0)
-{
+  {
     $anatype = $anadef if !defined $anatype || $anatype eq "";
     $com = $comdef if !defined $com;
-    $re = "" if !defined $re;
     
     if (!defined $run || $run eq "")
-    {
+      {
 	die "Must specify --run=<run number>\n";
-    }
-
+      }
+    
     @fn = (MakeTaFileName2 ($run, $anatype, "result", $com, ""));
-}
+  }
 else
-{
+  {
     @fn = @ARGV;
-}
+  }
 
 foreach $fn (@fn)
-{
+  {
     open (FILE, $fn) or die "Can't open file $fn";
     warn "Opened $fn for input\n";
     
     $foundheader = 0;
     while (<FILE>)
-    {
-	next if /^\s*$/ || /^\#/;  # skip blanks and comments
+      {
+	next if /^\s*$/ || /^\#/; # skip blanks and comments
 	if (!$foundheader)
-	{
+	  {
 	    $foundheader = 1;
-	    next; # skip header
-	}
+	    next;		# skip header
+	  }
 	
 	if (/$re/)
-	{
-	    ($stage, $qty, $stat, $rest) = /(.*?) ([^ ]*)_(.*?) (.*)/;
+	  {
+	    ($stage, $qtystat, $rest) = split /\s+/, $_, 3;
+	    if ($qtystat =~ /_/)
+	      {
+		($qty, $stat) = $qtystat =~ /(.*)_(.*)/;
+	      }
+	    else
+	      {
+		($qty, $stat) = ($qtystat, "")
+	      }
 	    
 	    ($val, $err, $min, $max, $units) = split (" ", $rest, 5);
 	    
 	    $key = "${stage}~${qty}~${min}~$max";
 	    
 	    if ($stat eq "mean")
-	    {
+	      {
 		$mean{$key} = (sprintf "%.3f", $val) . " +- " . (sprintf "%.3f", $err);
 		$units{$key} = "$units";
-	    }
+	      }
 	    elsif ($stat eq "RMS")
-	    {
+	      {
 		$rms{$key} = sprintf "%.3f", $val;
-	    }
+	      }
 	    elsif ($stat eq "Neff")
-	    {
+	      {
 		$neff{$key} = sprintf "%.1f", $val;
-	    }
-	}
-    }
+	      }
+	  }
+      }
     
     foreach $k (sort keys %mean)
-    {
+      {
 	($stage, $qty, $min, $max) = split ("~", $k);
 	print "From ${stage}: $qty in evts $min to $max: ";
 	print "  mean $mean{$k}";
@@ -94,20 +102,20 @@ foreach $fn (@fn)
 	print " $units{$k}";
 	print " with N_eff = $neff{$k}" if defined $neff{$k};
 	print "\n";
-    }
-}
+      }
+  }
 
 sub dieusage
-{
+  {
     die << "USE"
 usage: rut [options] [files]
-   Options to locate result files:
-	 --a=s or --anatype=s   analysis type (defaults to "$anadef")
-	 --c=s or --comment=s   comment (defaults to "$comdef")
-	 --r=i or --run=i       run number (required if no files specified)
+Options to locate result files:
+   --a=s or --anatype=s   analysis type (defaults to "$anadef")
+   --c=s or --comment=s   comment (defaults to "$comdef")
+   --r=i or --run=i       run number (required if no files specified)
 
-   Other options:
-	 --e=s or --expr=s      regular expression to look for
-	 --h   or --help        print this message
+Other options:
+   --e=s or --expr=s      regular expression to look for
+   --h   or --help        print this message
 USE
-}
+  }
