060a6c45a73f6b4bd0eeb38aa8bb54b371153aaf
rsholmes <rsholmes>
2004-06-01 00:37:18 +0000

    Heavy rewrite to handle Redana; command line options have changed -- do prompt.pl -? to see usage

--- utils/prompt.pl
+++ utils/prompt.pl
@@ -3,14 +3,28 @@
 # HAPPEX prompt analysis script
 
 # usage:
-#  prompt.pl [-f] [run]
-#  prompt.pl [-f] -b [list of runs]
+#  prompt.pl [option] [run]
+#  prompt.pl [option] --batch [list of runs]
 
-# Option -f forces Pan analysis of the CODA file even if a ROOT file
-# already exists; otherwise Pan analysis is skipped.  Then we run 
-# prompt.macro from within Pan.
+# Options are
+#
+#   --forcepan
+#   --forcereg
+#   --forcedit
+#   --nopan
+#   --noreg
+#   --nodit
+#   --nomacro
 
-# Option -b means batch mode: no graphics windows are opened,
+# Normally Pan analysis of the CODA file is skipped if a Pan ROOT file
+# already exists (in the $PAN_ROOT_FILE_PATH), regression and
+# dithering analyses are skipped if a Redana ROOT file already exists,
+# and prompt.macro is run (from inside ROOT).  The above options
+# force Pan analysis, regression analysis, or dithering analysis to be
+# done in any case (--forcexxxx) or force these or prompt.macro
+# execution to be skipped (--noxxxx).
+
+# Option --batch means batch mode: no graphics windows are opened,
 # prompt.macro is called with nonzero second argument, and Pan exits
 # after prompt.macro completes.  Otherwise prompt is called with zero
 # second argument and Pan stays active so user can inspect plots.
@@ -22,26 +36,33 @@ use lib $FindBin::Bin;
 use TaFileName;  # put the directory containing this (./utils) in PERL5LIB environment variable
                  # if you want to run this from a different directory
 
-$force = "";
+$DEBUG = 0;
+
+$forcepan = "";
+$forcereg = "";
+$forcedit = "";
+$nopan = "";
+$noreg = "";
+$nodit = "";
+$nomacro = "";
 $batch = 0;
 $opts = "";
-while ($#ARGV >= 0 && $ARGV[0] =~ /^-/)
+
+# The following greatly evil code just gets defined options off the
+# command line and sets the corresponding variables to 1.
+
+%opts = qw / forcepan x forcereg x forcedit x nopan x noreg x nodit x nomacro x batch x /;
+eval "\$$1=1" while $#ARGV > -1 && $ARGV[0] =~ /^--(\w+)/ && $opts{$1} && shift; 
+die &usage() if $#ARGV > -1 && $ARGV[0] =~ /^-/;
+
+
+if ($batch)
 {
-    if ($ARGV[0] eq '-f')
-    {
-	shift;
-	$force = 1;
-    }
-    elsif ($ARGV[0] eq '-b')
-    {
-	shift;
-	$batch = 1;
-	$opts = "-b -q";
-    }
-    else
-    {
-	die &usage();
-    }
+    $opts = "-b -q";
+}
+else
+{
+    $batch = 0;
 }
 
 if (!$batch && $#ARGV < 0)
@@ -56,7 +77,7 @@ else
 }
 
 die "No runs to analyze" if $#runs < 0;
-die "Multiple runs without -b option" if !$batch && $#runs > 0;
+die "Multiple runs without --batch option" if !$batch && $#runs > 0;
 
 print "Analyzing run" . ($#runs > 0 ? "s" : "") . " " . (join ", ", @runs) . "\n";
 foreach $run (@runs)
@@ -66,33 +87,106 @@ foreach $run (@runs)
 	warn "\n'$run' is not a valid run number, skipping\n";
 	next;
     }
+
     warn "\n============ Attempting Prompt analysis for run $run\n";
     $codafn = (MakeTaFileName2 ($run, "", "coda", "", ""));
     $rootfn = (MakeTaFileName2 ($run, "standard", "root", "", ""));
-    if ($force || !-e $rootfn)
-    { 
-	if (!-e $codafn)
-	{
-	    warn "CODA file $codafn not found, skipping\n";
-	    next;
+    $rrootfn = (MakeTaFileName2 ($run, "red", "root", "", ""));
+
+# Pan analysis =======================
+
+    if (!$nopan)
+    {
+	if ($forcepan || !-e $rootfn)
+	{ 
+	    if (!-e $codafn)
+	    {
+		warn "CODA file $codafn not found, Pan analysis not done\n";
+	    }
+	    else	
+	    {	    
+		system "echo './pan -r $run'";
+		system "./pan -r $run" if !$DEBUG;
+	    }
 	}
-	system "echo './pan -r $run'";
-	system "./pan -r $run";
+	else
+	{
+	    warn "Pan ROOT file $rootfn already exists, Pan analysis not done\n";
+	}
     }
-    else
-    {
-	warn "ROOT file $rootfn already exists\n";
+
+# Regression analysis =======================
+
+    if (!$noreg) 
+    { 
+	if ($forcereg || !-e $rrootfn) 
+	{ 
+	    if (!-e $rootfn) 
+	    {
+		warn "Pan ROOT file $rootfn not found, regression analysis not done\n"; 
+	    } 
+	    else
+	    {
+		# Note that the following system commands contain a shell
+		# metacharacter (;) so are run in sh.  A simple command
+		# with no metacharacter would be handled differently!!
+		
+		system "echo 'export PAN_CONFIG_FILE_SUFFIX=cfg_reg; ./redana -r $run'";
+		system "export PAN_CONFIG_FILE_SUFFIX=cfg_reg; ./redana -r $run" if !$DEBUG;
+	    }
+	}
+	else
+	{
+	    warn "Redana ROOT file $rrootfn already exists, regression analysis not done\n";
+	}
     }
-    if (!-e $rootfn)
-    {
-	warn "ROOT file $rootfn not found, skipping\n";
-	next;
+
+# Dithering analysis =======================
+
+    if (!$nodit) 
+    { 
+	if ($forcedit || !-e $rrootfn) 
+	{ 
+	    if (!-e $rootfn) 
+	    {
+		warn "Pan ROOT file $rootfn not found, dithering analysis not done\n";
+	    } 
+	    else
+	    {
+		# Note that the following system commands contain a shell
+		# metacharacter (;) so are run in sh.  A simple command
+		# with no metacharacter would be handled differently!!
+		
+		system "echo 'export PAN_CONFIG_FILE_SUFFIX=cfg_dit; ./redana -r $run'";
+		system "export PAN_CONFIG_FILE_SUFFIX=cfg_dit; ./redana -r $run" if !$DEBUG;
+	    }
+	}
+	else
+	{
+	    warn "Redana ROOT file $rrootfn already exists, dithering analysis not done\n";
+	}
     }
-    system "echo \"./pan $opts 'prompt.macro($run, $batch)'\"";
-    system "./pan $opts 'prompt.macro($run, $batch)'";
+
+
+# prompt.macro =======================
+
+    if (!$nomacro) 
+    { 
+	system "echo \"root $opts 'prompt.macro($run, $batch)'\"";
+	system "root $opts 'prompt.macro($run, $batch)'" if !$DEBUG;
+    }
 }
 
 sub usage
 {
-    return "Usage: prompt.pl [-f] -b [list of runs]\n       prompt.pl [-f] [run]\n";
+    return <<USAGE;
+Usage: prompt.pl [options] --batch [list of runs]
+       prompt.pl [options] [run]
+Options are 
+
+       --forcepan --forcereg --forcedit  force Pan, regression,
+                                           or dither analyses;
+       --nopan --noreg --nodit           skip them;
+       --nomacro                         skip prompt.macro
+USAGE
 }
